2c3efc2c0d115215c01b8f0a5c994105737ce07d
Kevin <kcoxe@keywcorp.com>
2016-01-11 13:59:18 -0500

    edit file name for install

---
+++ install.sh
@@ -0,0 +1,14 @@
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# create softlinks
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# install plugins
+vim +PluginInstall +qall
+
+python2 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer --tern-completer
+
