f11967d858a9997137c79f122bc197ac0963dbcd
Kevin Coxe <kevcoxe@gmail.com>
2017-01-19 17:39:22 -0500

    remove dot from file names

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # create softlinks
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
 # install plugins
 echo "" | vim +PluginInstall +qall
