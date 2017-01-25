d89b89a49d5f78680ab07bbd35219d9a3127304b
Kevin Coxe <kevcoxe@gmail.com>
2017-01-25 18:11:35 -0500

    copy over ycm file

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # create softlinks
 ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/ycm_extra_conf.py ~/.ycm_extra_conf
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
